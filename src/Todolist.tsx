e77fcce11754dc9d3cc9ca86f2a1d86af6be0264
Daniil_Shatiev <shatiev_daniil@mail.ru>
2024-09-01 20:15:25 +0400

    three components with different titles

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const Todolist = () => {
+export const Todolist = (props: any) => {
     return <div>
-        <h3>What to learn</h3>
+        <h3>{props.title}</h3>
         <div>
             <input/>
             <button>+</button>
